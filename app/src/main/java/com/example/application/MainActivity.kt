ba7656b523146e501f3b9b74abe184540f897a2d
gleroy <guillaume.leroy34@gmail.com>
2023-09-27 10:21:57 +0200

    ajout de l'image

--- app/src/main/java/com/example/application/MainActivity.kt
+++ app/src/main/java/com/example/application/MainActivity.kt
@@ -1,15 +1,22 @@
   package com.example.application
 
+import android.graphics.drawable.shapes.Shape
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.application.ui.theme.ApplicationTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,6 +27,7 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
                     Greeting("Android")
+                    MonImage()
                 }
             }
         }
@@ -32,6 +40,20 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
             text = "Hello $name!",
             modifier = modifier
     )
+}
+
+@Composable
+fun MonImage(){
+    Image(
+        painterResource(id = R.drawable.chat),
+    contentDescription = "le chat",
+    modifier = Modifier.padding(10.dp).clip(CircleShape)
+    )
+}
+h
+@Composable
+fun Legende(){
+
 }
 
 @Preview(showBackground = true)
